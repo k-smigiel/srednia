e30864274e072ef37e99f3ecc7ec872a3690c662
k-smigiel <kacper.smigiel1@gmail.com>
2022-03-06 15:41:57 +0100

    zmiana wag 0.4->0.6, 0.6->0.4

--- avg.php
+++ avg.php
@@ -1,8 +1,6 @@
 <?php
-
-<?php
-$oceny = array(3, 5, 2, 1, 4, 4);
-$srednia = array_sum($oceny)/count($oceny);
-echo round($srednia,1);
-?>
+function srednia($a, $b)
+{
+		return 0.4*$ab = 0.6*b;
+}
 ?>
